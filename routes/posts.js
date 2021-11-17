1c7e0f0df76007087313ca2268af990afd810144
Daniel Kissner <danielkissner@Daniels-Air.attlocal.net>
2021-11-17 09:56:27 -0500

    added a json response to the posts route

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const verify = require("../routes/verifyToken");
 
 router.get("/", verify, (req, res) => {
-  res.send(req.user);
+  res.json({ posts: [{ title: "title", content: "content" }] });
   //   res.json({ posts: [{ title: "my first posts", content: "content" }] });
 });
 
